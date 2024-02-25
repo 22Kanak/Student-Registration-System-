cd6c4551da3654f88ebe678773946a1ced770d4f
Kanak Thakur <142377296+22Kanak@users.noreply.github.com>
2024-02-25 20:08:13 +0530

    Update Connectivity.java

--- src/utility/Connectivity.java
+++ src/utility/Connectivity.java
@@ -1,5 +1,29 @@
 package utility;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Connectivity {
-
+ public static Connection getConnection() throws SQLException {
+		 
+		 Connection connection = null ;
+		 
+		 try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 String url = "jdbc:mysql://localhost:3306/Automated_Student_Registration_System";
+		 
+		 try {
+			connection = DriverManager.getConnection(url,"root","Kanakk@123");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new SQLException(e.getMessage());
+		}
+		 
+		return connection;
+		 
+	 }
 }
